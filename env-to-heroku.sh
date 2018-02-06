fccf140beff9a967761a587f30ba9d9ede199b36
Clément Delmas <clement.delmas@gmail.com>
2018-02-07 00:00:41 +0100

    [Heroku] Add bash script to deploy .env variables to heroku

---
+++ env-to-heroku.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Check .env file
+if [ ! -f ./.env ]; then
+    echo "File .env not found!"
+    exit
+fi
+
+# Initialize
+heroku_set="heroku config:set "
+
+# Parse .env file and loop on file lines
+while read l; do
+  # Check for empty or comments lines
+  if [[ -z $l ]] || [[ $l == \#* ]] ;
+  then
+    continue
+  fi
+
+  # Check for dev environment and convert to prod
+  if [[ $l == "APP_ENV=dev" ]] ;
+  then
+    l="APP_ENV=prod"
+  fi
+
+  # Remove quotes
+  l=$(echo "$l" | tr -d "'")
+
+  # Add config to line
+  heroku_set="$heroku_set $l"
+done <.env
+
+# Execute Heroku set config
+eval "$heroku_set"
