b935f7e6f72a47d9e760fd436a0780ac28b107e6
Clément Delmas <clement.delmas@gmail.com>
2018-02-08 09:10:50 +0100

    [Entity/SlackLink] Try to fix auto-increment PostgreSQL bug

--- src/Entity/SlackLink.php
+++ src/Entity/SlackLink.php
@@ -11,7 +11,7 @@ class SlackLink
 {
     /**
      * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      * @ORM\Column(type="integer")
      */
     private $id;
