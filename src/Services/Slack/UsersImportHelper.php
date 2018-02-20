eaf351b208ba813ea4cb20581117da8884fa677d
Clément Delmas <clement.delmas@gmail.com>
2018-02-20 17:28:35 +0100

    [Services/Slack] Refactor importUsersList() to lower the complexity

--- src/Services/Slack/UsersImportHelper.php
+++ src/Services/Slack/UsersImportHelper.php
@@ -55,26 +55,39 @@ class UsersImportHelper extends ImportHelper
                 continue;
             }
 
-            // Initialize
-            $userRealName = empty($rawUser->real_name) ? $rawUser->name : $rawUser->real_name;
-
-            // Create SlackUser
-            $user = new SlackUser();
-            $user->setSlackId($rawUser->id);
-            $user->setUsername($rawUser->name);
-            $user->setRealName($userRealName);
-
-            // Check for profile and avatar
-            if (isset($rawUser->profile->image_original)) {
-                $user->setAvatar($rawUser->profile->image_original);
-            }
-
-            // Persist user
-            $this->em->persist($user);
+            // Initialize and persist user
+            $this->em->persist($this->initializeSlackUserFromRawUser($rawUser));
             $this->em->flush();
         }
 
         // Return success
         return true;
+    }
+
+    /**
+     * Get Slack User from a parsed user
+     *
+     * @param \stdClass $rawUser - Raw user
+     *
+     * @return SlackUser - Initialized SlackUser
+     */
+    private function initializeSlackUserFromRawUser($rawUser) : SlackUser
+    {
+        // Initialize
+        $userRealName = empty($rawUser->real_name) ? $rawUser->name : $rawUser->real_name;
+
+        // Create SlackUser
+        $user = new SlackUser();
+        $user->setSlackId($rawUser->id);
+        $user->setUsername($rawUser->name);
+        $user->setRealName($userRealName);
+
+        // Check for profile and avatar
+        if (isset($rawUser->profile->image_original)) {
+            $user->setAvatar($rawUser->profile->image_original);
+        }
+
+        // Return initialized user
+        return $user;
     }
 }
